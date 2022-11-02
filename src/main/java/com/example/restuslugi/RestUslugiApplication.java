a0bd57291de86b94c340df8423e014692edc1c43
ISParashkevov18 <ISParashkevov18@codingburgas.bg>
2022-11-02 15:40:38 +0200

    Initial commit

---
+++ src/main/java/com/example/restuslugi/RestUslugiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restuslugi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestUslugiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestUslugiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restuslugi/RestUslugiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restuslugi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestUslugiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
